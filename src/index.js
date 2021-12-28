5b71c358c0045ee14a3c88fabe75eccb1358ffd0
gioadorno <adorno.gio@gmail.com>
2021-12-28 08:12:04 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index';
+import { applyMiddleware, createStore, compose } from 'redux';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
